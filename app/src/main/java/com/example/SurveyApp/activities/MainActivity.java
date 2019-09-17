b661295cae419564d006259e9e202f53818bf111
Robin <nasimulhaque.robin@gmail.com>
2019-09-17 12:38:12 +0600

    init

--- app/src/main/java/com/example/SurveyApp/activities/MainActivity.java
+++ app/src/main/java/com/example/SurveyApp/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.SurveyApp.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -200,5 +203,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 //                Bitmap selectedImage = BitmapFactory.decodeFile(filePath);
             }
         }
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
